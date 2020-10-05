5bf19423b6cc53d9d414e82b4a7a9c31ca75915f
BRODAR <exonet3i@gmail.com>
2020-10-05 15:26:59 +0200

    promo commit

---
+++ js/count.js
@@ -0,0 +1,15 @@
+ var countDownDate = new Date("Nov 6, 2020 23:59:59").getTime();    
+ var x = setInterval(function() { 
+   var now = new Date().getTime();     
+   var distance = countDownDate - now;     
+   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+   var seconds = Math.floor((distance % (1000 * 60)) / 1000);     
+document.getElementById("count").innerHTML = days + "j " + hours + "h "
+   + minutes + "m " + seconds + "s ";     
+     if (distance < 0) {
+     clearInterval(x);
+     document.getElementById("count").innerHTML = "EXPIRED";
+   }
+ }, 1000);
