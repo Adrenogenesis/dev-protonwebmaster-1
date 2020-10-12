705cbde26ac54d3199c4635b61e323dbe693d3d3
protonwebmaster <exonet3i@gmail.com>
2020-10-12 08:52:34 +0200

    game commit

--- public/js/count.js
+++ public/js/count.js
@@ -1,4 +1,4 @@
- var countDownDate = new Date("Nov 6, 2020 23:59:59").getTime();    
+ var countDownDate = new Date("Nov 12, 2020 23:59:59").getTime();    
  var x = setInterval(function() { 
    var now = new Date().getTime();     
    var distance = countDownDate - now;     
