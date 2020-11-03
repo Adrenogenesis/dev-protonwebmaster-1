1215940109f96ff1aec3c12307759f40eeb9d5c3
BRODAR <exonet3i@gmail.com>
2020-11-03 11:57:29 +0100

    game commit

--- js/count.js
+++ js/count.js
@@ -1,4 +1,4 @@
- var countDownDate = new Date("Nov 12, 2020 23:59:59").getTime();    
+ var countDownDate = new Date("Dec 15, 2020 23:59:59").getTime();    
  var x = setInterval(function() { 
    var now = new Date().getTime();     
    var distance = countDownDate - now;     
